de85fc3c0c2cc54d8b6f3f1382bcae3cca01ccf5
Replit user <>
2023-01-12 09:13:33 +0000

    Completed assignmentgit status

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+	// const firstTwoDrivers = [];
+	return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+	return drivers.slice(-2)
+}
+
+const selectingDrivers = [
+	returnFirstTwoDrivers, returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(multiplier) {
+	return function(fare) {
+		return fare * multiplier;
+	};
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnTwoDrivers){
+	return returnTwoDrivers(drivers);
+}
+
